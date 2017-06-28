3f13d8c35154de1d1522fe35ad1bae06f872d7c8
Huff <Huff@lhfpc>
2017-06-28 17:27:54 +0800

    Java Shiro实现权限安全认证实例

---
+++ src/com/lhf/shiro/handlers/ShiroHandler.java
@@ -0,0 +1,39 @@
+package com.lhf.shiro.handlers;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.annotation.RequiresRoles;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ShiroHandler {
+
+	@RequiresRoles("list")
+	@RequestMapping("/list")
+	public String list(){
+		System.out.println("...list...");
+		return "list";
+	}
+	
+	@RequestMapping("/shiro-login")
+	public String login(@RequestParam("username") String username, 
+			@RequestParam("password") String password){
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+		
+		try {
+        	//执行认证操作. 
+            subject.login(token);
+        }catch (AuthenticationException ae) {
+        	System.out.println("登陆失败: " + ae.getMessage());
+        	return "/shiro-login";
+        }
+		
+		return "/shiro-success";
+	}
+	
+}
